d988a20cba488c3dbc75589ef19458d2eed2bd05
Alessandropiroddi <alessandro.piroddi2001@gmail.com>
2025-04-02 19:25:26 +0200

    #1

--- app/classes/preprocessor.py
+++ app/classes/preprocessor.py
@@ -10,6 +10,5 @@ class Preprocessor:
 
     def clean_string(self,string):
 
-       string = re.sub(r"[^A-Za-z 28]",'',string)
-
-       return string
+       string = re.sub(r"[^A-Za-z 28]",'',string)      #Ho usato re.sub in modo che sostituisca con '' (perciò rimuova)
+       return string                                               #qualsiasi carattere che non sia una lettera o uno spazio o i numeri 2 e 8
